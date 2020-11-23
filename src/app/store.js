ee98df2cc11ea98c2689a5b2aed4b0d3f72ffb5d
Vladimir Komarov <34229011+thelonerover@users.noreply.github.com>
2020-11-24 02:57:53 +0300

    init stuff

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
 import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
