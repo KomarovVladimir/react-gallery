aebe371673bde0525f073b288aba34c76ca3be71
Vladimir Komarov <34229011+thelonerover@users.noreply.github.com>
2020-11-27 07:05:15 +0300

    predeployment

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import AlbumList from '../AlbumsList';
 import ImagesList from '../ImagesList';
 import typography from '../../scss/typography.module.scss';
 
 function App() {
-  // const currentAlbumTitle = useSelector(state => state.albums.currentAlbum.title);
-
   return (
-    <Router>
+    <Router basename="/catalog-spa">
       <Switch>
         <Route exact path='/'>
           <h1 className={typography.h1}>Albums</h1>
