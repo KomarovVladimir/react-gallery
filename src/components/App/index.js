c9aff41aa315c7a468450e24fce31d151fb9e70a
Vladimir Komarov <34229011+thelonerover@users.noreply.github.com>
2020-11-30 04:02:20 +0300

    loader

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import AlbumList from '../AlbumsList';
 import ImagesList from '../ImagesList';
+import Loader from '../Loader';
 import typography from '../../scss/typography.module.scss';
 
 function App() {
+  const isLoading = useSelector(state => state.albums.isLoading);
+
   return (
     <Router basename="/react-gallery">
       <Switch>
         <Route exact path='/'>
           <h1 className={typography.h1}>Albums</h1>
+          {isLoading && <Loader />}
           <AlbumList />
         </Route>
         <Route path='/album'>
           <h1 className={typography.h1}><Link className={`${typography.h1} ${typography.a}`} to='/'>Back to albums</Link></h1>
+          {isLoading && <Loader />}
           <ImagesList />
         </Route>
       </Switch>
